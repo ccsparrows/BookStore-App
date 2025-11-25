1f18fd3beb8c979cf94c5509e1978a48e2dec3a5
ccsparrow <2397299116@qq.com>
2025-11-25 14:16:51 +0800

    Initial commit

---
+++ mobile/app/_layout.jsx
@@ -0,0 +1,63 @@
+import { SplashScreen, Stack, useRouter, useSegments } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import SafeScreen from "../components/SafeScreen";
+import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
+
+import { useAuthStore } from "../store/authStore";
+import { useEffect, useState } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const router = useRouter();
+  const segments = useSegments();
+
+  const { checkAuth, user, token } = useAuthStore();
+
+  const [fontsLoaded] = useFonts({
+    "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMono-Medium.ttf"),
+  });
+
+  const [authChecked, setAuthChecked] = useState(false);
+
+  useEffect(() => {
+    const doAuthCheck = async () => {
+      await checkAuth();
+      setAuthChecked(true);
+    };
+    doAuthCheck();
+  }, []);
+
+  useEffect(() => {
+    if (fontsLoaded && authChecked) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, authChecked]);
+
+  // Handle navigation AFTER everything is ready
+  useEffect(() => {
+    if (!authChecked || !fontsLoaded) return; // ⛔ Wait until ready
+
+    const inAuthScreen = segments[0] === "(auth)";
+    const isSignedIn = user && token;
+
+    if (!isSignedIn && !inAuthScreen) {
+      router.replace("/(auth)");
+    } else if (isSignedIn && inAuthScreen) {
+      router.replace("/(tabs)");
+    }
+  }, [authChecked, fontsLoaded, user, token, segments]);
+
+  return (
+    <SafeAreaProvider>
+      <SafeScreen>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="(auth)" />
+        </Stack>
+      </SafeScreen>
+      <StatusBar style="dark" />
+    </SafeAreaProvider>
+  );
+}
